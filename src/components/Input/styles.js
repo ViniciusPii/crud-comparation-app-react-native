6953ef69984842bd48b8d1161248ca32845856b1
Vinicius <vinicius@iMac-de-Vinicius.local>
2020-06-01 10:26:28 -0300

    Implementação do react-navigation

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -8,5 +8,7 @@ export const Input = styled.TextInput.attrs(({theme}) => ({
   margin-bottom: 20px;
   padding-left: 10px;
   border-radius: 8px;
+  font-size: 18px;
+  color: ${({theme}) => theme.neutral800};
   background-color: ${({theme}) => theme.placeholderNeutral};
 `;
