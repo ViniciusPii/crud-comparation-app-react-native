dcf1a77186f76c7a40fed321a3a6e56008c6ea45
Vinicius <vinicius@iMac-de-Vinicius.local>
2020-06-04 10:30:59 -0300

    Autenticação completa por context

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -14,7 +14,6 @@ const AuthProvider = ({children}) => {
       setSigned(!!user);
     });
   });
-  //////////
 
   // loga usuário
   const login = (email, password) => {
@@ -38,11 +37,58 @@ const AuthProvider = ({children}) => {
         setLoading(false);
       });
   };
-  ///////////
+
+  // cria usuário
+  const createAccount = (name, email, password) => {
+    setLoading(true);
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(res => {
+        //separação do id e criação da chave para o item
+        const {uid} = res.user;
+
+        //criando o nó admin
+        firebase
+          .database()
+          .ref('admin')
+          .child(uid)
+          .set({
+            name,
+            email,
+          });
+
+        alert('Cadastrado com sucesso!');
+        setLoading(false);
+      })
+      .catch(erro => {
+        if (name === '' || email === '' || password === '') {
+          alert('Preencha todos os campos!');
+          setLoading(false);
+          return;
+        }
+
+        switch (erro.message) {
+          case 'Password should be at least 6 characters':
+            alert('Sua senha deve ter no minimo 6 caracteres!');
+            break;
+          case 'The email address is already in use by another account.':
+            alert('Esse email já está sendo utilizado por outro usuário!');
+            break;
+          case 'The email address is badly formatted.':
+            alert('O formato do email não é válido!');
+            break;
+          default:
+            alert('Não foi possível cadastrar o Usuário');
+            break;
+        }
+        setLoading(false);
+      });
+  };
 
   return (
     <AuthContext.Provider
-      value={{loading, setLoading, signed, setSigned, login}}>
+      value={{loading, setLoading, signed, setSigned, login, createAccount}}>
       {children}
     </AuthContext.Provider>
   );
@@ -50,8 +96,8 @@ const AuthProvider = ({children}) => {
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
-  const {signed, loading, login} = context;
-  return {signed, loading, login};
+  const {signed, loading, login, createAccount} = context;
+  return {signed, loading, login, createAccount};
 };
 
 export default AuthProvider;
--- src/views/Auth/CreateAccount/index.js
+++ src/views/Auth/CreateAccount/index.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 
-import firebase from '../../../services/firebase';
+import {useAuth} from '../../../contexts/AuthContext';
 
 import {
   Layout,
@@ -12,57 +12,14 @@ import {
 } from '../../../components';
 
 const CreateAccount = () => {
+  const {loading, createAccount} = useAuth();
+
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const [loading, setLoading] = useState(false);
-
   const handleCreateAccount = () => {
-    setLoading(true);
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(email, password)
-      .then(res => {
-        //separação do id e criação da chave para o item
-        const {uid} = res.user;
-
-        //criando o nó admin
-        firebase
-          .database()
-          .ref('admin')
-          .child(uid)
-          .set({
-            name,
-            email,
-          });
-
-        alert('Cadastrado com sucesso!');
-        setLoading(false);
-      })
-      .catch(erro => {
-        if (name === '' || email === '' || password === '') {
-          alert('Preencha todos os campos!');
-          setLoading(false);
-          return;
-        }
-
-        switch (erro.message) {
-          case 'Password should be at least 6 characters':
-            alert('Sua senha deve ter no minimo 6 caracteres!');
-            break;
-          case 'The email address is already in use by another account.':
-            alert('Esse email já está sendo utilizado por outro usuário!');
-            break;
-          case 'The email address is badly formatted.':
-            alert('O formato do email não é válido!');
-            break;
-          default:
-            alert('Não foi possível cadastrar o Usuário');
-            break;
-        }
-        setLoading(false);
-      });
+    createAccount(name, email, password);
   };
 
   return (
